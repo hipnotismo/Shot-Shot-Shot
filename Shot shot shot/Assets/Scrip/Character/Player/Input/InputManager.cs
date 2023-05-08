46c61888d92c58d0dd726ba264df3ac370e2f65b
martin <martinconcetti97@gmail.com>
2023-05-07 22:55:04 -0300

    Menu bug, player prefab and tutorial

---
+++ Shot shot shot/Assets/Scrip/Character/Player/Input/InputManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.LowLevel;
+
+public class InputManager : MonoBehaviour
+{
+    [Header("References")]
+    private CharacterMovement plMov;
+    private PlayerCamera plLook;
+    private PickUpWeapon pickUp;
+
+    public delegate void ShotAction();
+    public static event ShotAction ShootFromPickUp;
+
+    //private GunBase gun;
+
+    //hacer con eventos
+
+    void Start()
+    {
+        plMov = GetComponent<CharacterMovement>();
+        plLook = GetComponentInChildren<PlayerCamera>();
+        pickUp = GetComponent<PickUpWeapon>();
+
+    }
+
+    public void OnMove(InputValue value)
+    {
+        plMov.Movement(value);
+    }
+
+    public void OnLook(InputValue inputValue)
+    {
+        plLook.LookLogic(inputValue);
+    }
+
+    public void OnFire()
+    {
+        ShootFromPickUp();
+    }
+
+    public void OnPickUp()
+    {
+       // plMov.Interact();
+    }
+
+}
